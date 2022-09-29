e2caf10f65989c3cdf5ea62f7c9c1178a412462c
Eniola Hannah <heniola395@gmail.com>
2022-09-29 21:54:08 +0100

    Created function withdraw

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 var myBalance = 1000
-display.innerHTML = `Welcome; <br> You have a default balance of 1000`
+display.innerHTML = `Welcome; <br> You have a default balance of $1000`
+const withdraw=()=>{
+    if (inputAmount.value == ""){
+        display.innerHTML = "Input a value"
+    }
+    else if(inputAmount.value<=myBalance){
+        myBalance = myBalance - inputAmount.value
+        display.innerHTML = `succesfully withdrawn ${inputAmount.value} <br> New balance = $ ${myBalance}`
+    } 
+    else{
+        display.innerHTML = `Eeyah, Insufficient Balance`
+    }
+}
+
